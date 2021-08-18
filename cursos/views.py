10644104047e672ee022d5d4cadc03af7563ae55
andre <andd68@hotmail.com>
2021-08-18 11:37:28 -0300

    add PUT and DELETE using generics classes

--- cursos/views.py
+++ cursos/views.py
@@ -1,50 +1,25 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Curso, Avaliacao
-from .serializers import CursoSerealizer, AvaliacaoSerializer
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """
-    API de cursos teste
-    """
-    def get(self, request):  # http GET
-        # print(request)
-        cursos = Curso.objects.all()
-        serializer = CursoSerealizer(cursos, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CursoSerealizer(data=request.data)  # recebe dados do body da request
-        serializer.is_valid(raise_exception=True)  # verifica se a requisição para a API é valida
-        serializer.save()  # salva os dados obtidos no banco de dados
-
-        # ao criar retorna o objeto criado no BD
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        # retorna a mensagem
-        # return Response({"msg": "Objeto criado com sucesso"}, status=status.HTTP_201_CREATED)
-
-        # retorna apenas o id e o nome do curso
-        # return Response({"id": serializer.data['id'],
-        # "curso": serializer.data['titulo']}, status=status.HTTP_201_CREATED)
+# ListCreateAPIView ---> GET e POST ---->listar coleção e criar objeto
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
 
-class AvaliacaoAPIView(APIView):
-    """
-    API de avaliações teste
-    """
-    def get(self, request):
-        # print(dir(request))
-        # print(request.user)
-        # print(request.user.id)
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
+# RetrieveUpdateDestroyAPIView -----> necessario ID ----> update e delete
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
