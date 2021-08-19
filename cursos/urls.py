76937ff28b07daceced4152d475958ab87531e9b
andre <andd68@hotmail.com>
2021-08-19 09:37:32 -0300

    get_queryset and get_object

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,7 +3,15 @@ from .views import CursoAPIView, AvaliacaoAPIView, CursosAPIView, AvaliacoesAPIV
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
+
+    # curso_pk ----> explicita que a primary key a obter é de curso
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+
+    # como um método foi sobrescrito foi necessario mudar a chamada da pk em avaliacao
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
@@ -19,7 +21,26 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    # metodo retorna uma lista de objetos
+    def get_queryset(self):
+
+        # cursos/curso_pk/avaliacoes
+        # verifica se na url há curso_pk
+        if self.kwargs.get('curso_pk'):
+
+            # se houver atribui id a pk e procura no bd
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+
+        return self.queryset.all()  # se não houver retorna todas avaliações
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    # retorna objeto com determinada pk
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
