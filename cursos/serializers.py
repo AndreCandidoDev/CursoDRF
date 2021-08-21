01934c365daa5925eb871f2eb2f46e43fe2da8d0
andre <andd68@hotmail.com>
2021-08-21 10:39:20 -0300

    Validação customizada no serializer

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -28,6 +28,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'ativo',
         )
 
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):  # 1, 2, 3, 4, 5
+            return valor
+        raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
+
 
 class CursoSerializer(serializers.ModelSerializer):
 
